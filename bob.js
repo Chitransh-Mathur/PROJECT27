6c7e83452c8b25a9259bd23c61898718ee746fdf
Chitransh-Mathur <73891737+Chitransh-Mathur@users.noreply.github.com>
2020-12-19 20:52:49 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,40 @@
+
+class Bob
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:1,
+			friction:0,
+			density:0.9
+
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options);
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var bobpos=this.body.position;		
+
+			push()
+			translate(bobpos.x, bobpos.y);
+			rectMode(CENTER)
+			
+			fill(255,0,255)
+			ellipseMode(CENTER);
+			ellipse(0,0,this.r, this.r)
+		
+			pop()
+			
+	}
+
+}
+  
